4b5612f69a5081f7b2174a3b21ab52f352342a1b
Alexander Silver <asilver7493@gmail.com>
2020-02-29 04:29:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,21 @@ def using_concat(arr1, arr2)
 end
 
 def using_insert(array, element)
-  array
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten (array)
+  array.flatten()
+end
+
+def using_delete(array, element)
+  array.delete(element)
+end
+
+def using_delete_at(array, int)
+  
 end
