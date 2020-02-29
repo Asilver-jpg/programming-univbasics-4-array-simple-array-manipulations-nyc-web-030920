12f1b54227478e28c9427b4229ecbb7fdceab462
Alexander Silver <asilver7493@gmail.com>
2020-02-29 04:27:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,3 +11,21 @@ def using_pop(array)
 end
 
 def pop_with_args
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(array, element)
+  array
+end
